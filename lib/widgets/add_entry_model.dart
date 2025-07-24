09554adf93f129d329b9d3facaa7f87da36898b5
Tanveer Ahmad <tanveer8268335@gmail.com>
2025-07-24 14:54:31 +0500

    model added

--- lib/widgets/add_entry_model.dart
+++ lib/widgets/add_entry_model.dart
@@ -1,139 +1,275 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'entry_field.dart';
-
-class AddEntryModal extends StatelessWidget {
+class AddEntryModal extends StatefulWidget {
   const AddEntryModal({super.key});
+  @override
+  State<AddEntryModal> createState() => _AddEntryModalState();
+}
+
+class _AddEntryModalState extends State<AddEntryModal> {
+  int _selectedType = 0; // 0 = Meal, 1 = Activity
+
+  @override
+  Widget build(BuildContext context) {
+    final double maxHeight = MediaQuery.of(context).size.height * 0.9;
+    return SafeArea(
+      top: false,
+      child: SizedBox(
+        height: maxHeight,
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.add_box_outlined,
+                        color: Color(0xFFBDBDBD),
+                        size: 22,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        'Add new entry',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Colors.black.withValues(alpha: 0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.cancel_outlined,
+                      color: Color(0xFFBDBDBD),
+                      size: 24,
+                    ),
+                    splashRadius: 22,
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Entry type',
+                        style: GoogleFonts.poppins(
+                          fontSize: 13,
+                          color: Colors.grey[700],
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF5F5F5),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () => setState(() => _selectedType = 0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: _selectedType == 0
+                                        ? Colors.black
+                                        : Colors.transparent,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.restaurant,
+                                        size: 18,
+                                        color: _selectedType == 0
+                                            ? Colors.white
+                                            : Colors.grey[400],
+                                      ),
+                                      const SizedBox(width: 6),
+                                      Text(
+                                        'Meal',
+                                        style: GoogleFonts.poppins(
+                                          color: _selectedType == 0
+                                              ? Colors.white
+                                              : Colors.black,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () => setState(() => _selectedType = 1),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: _selectedType == 1
+                                        ? Colors.black
+                                        : Colors.transparent,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.directions_run,
+                                        size: 18,
+                                        color: _selectedType == 1
+                                            ? Colors.white
+                                            : Colors.grey[400],
+                                      ),
+                                      const SizedBox(width: 6),
+                                      Text(
+                                        'Activity',
+                                        style: GoogleFonts.poppins(
+                                          color: _selectedType == 1
+                                              ? Colors.white
+                                              : Colors.black,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 18),
+                      EntryField(label: 'Time', hint: '12:30 PM'),
+                      EntryField(
+                        label: 'Glucose reading (mg/dL)',
+                        hint: 'Enter glucose reading',
+                      ),
+                      EntryField(
+                        label: 'Description',
+                        hint: 'What did you eat?',
+                      ),
+                      EntryField(
+                        label: 'Carbohydrates (gr)',
+                        hint: 'Enter carbs amount',
+                      ),
+                      EntryField(
+                        label: 'Protein (gr)',
+                        hint: 'Enter protein amount',
+                      ),
+                      EntryField(label: 'Fats (gr)', hint: 'Enter fats amount'),
+                      const SizedBox(height: 18),
+                      ElevatedButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          minimumSize: const Size.fromHeight(48),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          elevation: 0,
+                        ),
+                        child: Text(
+                          'Save entry',
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      OutlinedButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        style: OutlinedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(48),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          side: const BorderSide(color: Color(0xFFEAEAEA)),
+                        ),
+                        child: Text(
+                          'Cancel',
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EntryField extends StatelessWidget {
+  final String label;
+  final String hint;
+  const EntryField({super.key, required this.label, required this.hint});
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(
-        left: 20,
-        right: 20,
-        top: 24,
-        bottom: MediaQuery.of(context).viewInsets.bottom + 20,
-      ),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Add new entry',
-                  style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.close, color: Colors.black),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ],
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: GoogleFonts.poppins(
+              fontSize: 13,
+              color: Colors.grey[700],
+              fontWeight: FontWeight.w500,
             ),
-            const SizedBox(height: 12),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.restaurant,
-                      size: 18,
-                      color: Colors.white,
-                    ),
-                    label: Text(
-                      'Meal',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: const StadiumBorder(),
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      elevation: 0,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: OutlinedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.directions_run,
-                      size: 18,
-                      color: Colors.black,
-                    ),
-                    label: Text(
-                      'Activity',
-                      style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    style: OutlinedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      side: const BorderSide(color: Color(0xFFEAEAEA)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 18),
-            EntryField(label: 'Time', hint: '12:30 PM'),
-            EntryField(
-              label: 'Glucose reading (mg/dL)',
-              hint: 'Enter glucose reading',
-            ),
-            EntryField(label: 'Description', hint: 'What did you eat?'),
-            EntryField(label: 'Carbohydrates (gr)', hint: 'Enter carbs amount'),
-            EntryField(label: 'Protein (gr)', hint: 'Enter protein amount'),
-            EntryField(label: 'Fats (gr)', hint: 'Enter fats amount'),
-            const SizedBox(height: 18),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).pop(),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                minimumSize: const Size.fromHeight(48),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                elevation: 0,
+          ),
+          const SizedBox(height: 4),
+          TextField(
+            decoration: InputDecoration(
+              hintText: hint,
+              hintStyle: GoogleFonts.poppins(
+                color: Colors.grey[400],
+                fontSize: 14,
               ),
-              child: Text(
-                'Save entry',
-                style: GoogleFonts.poppins(fontWeight: FontWeight.w600),
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: Color(0xFFEAEAEA)),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: Color(0xFFEAEAEA)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: Color(0xFFBDBDBD)),
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 14,
+                vertical: 14,
               ),
             ),
-            const SizedBox(height: 10),
-            OutlinedButton(
-              onPressed: () => Navigator.of(context).pop(),
-              style: OutlinedButton.styleFrom(
-                minimumSize: const Size.fromHeight(48),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                side: const BorderSide(color: Color(0xFFEAEAEA)),
-              ),
-              child: Text(
-                'Cancel',
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
